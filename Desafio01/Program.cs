8da6236cdd090cbb56347f773af5a13315412bfc
Markos Borges <markous_k@protonmail.com>
2023-02-20 21:46:51 -0300

    first commit

---
+++ Desafio01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Desafios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            DESAFIO 01 - 17/FEV
+            Objetivo
+            Escreva um programa que conte a quantidade de palavras e caracteres que um determinado texto, escrito no Console, contém.
+
+            Resultado esperado
+            Digite um texto:
+
+            Este texto foi digitado no console
+
+            > 34 caracteres, 6 palavras
+
+            */
+            Console.WriteLine(" ======= DESAFIO 01 ====== ");
+            //var text = "Texto de teste";
+            int cont = 0;
+            Console.WriteLine("Digite um breve texto/frase:");
+            string text = Console.ReadLine();
+            Console.WriteLine(text);
+
+            //text = text.Trim();
+            int tam = text.Length;
+            string[] palavras = text.Split(new Char[] { '.', '?', '!', ' ', ';', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in palavras)
+            {
+                cont++;
+            }
+
+            Console.WriteLine("====================================================");
+            Console.WriteLine($"Seu texto contém: {tam} caracteres, {cont} palavras.");
+            Console.WriteLine("====================================================");
+
+        }
+    }
+}
---
+++ Desafio02/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Desafios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            DESAFIO 02 - 18/FEV
+            Objetivo
+            Escreva um programa que calcula o IMC (Índice de massa corporal de uma pessoa) e exibe o resultado.
+
+            Resultado esperado
+            Informe sua altura: 1,75
+            Informe seu peso: 85
+
+            > Seu IMC é 27.76
+            > Sobrepeso
+            > Obesidade Grau I
+            > Risco: Aumentado
+
+            */
+            Console.Clear();
+            Console.WriteLine(" ======= DESAFIO 02 ====== ");
+
+            Console.WriteLine("Informa e sua altura:");
+            var altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o seu peso:");
+            var peso = double.Parse(Console.ReadLine());
+
+            var IMC = peso / (altura * 2);
+
+            if (IMC < 16.0)
+            {
+                Console.WriteLine($"Seu IMC é {Math.Round(IMC, 2)}");
+                Console.WriteLine("Magreza Grau III");
+                Console.WriteLine("Você esta muito abaixo do peso!");
+                Console.WriteLine("Risco: ");
+            }
+            else if (IMC >= 16.0 && IMC <= 16.9)
+            {
+                Console.WriteLine($"Seu IMC é {Math.Round(IMC, 2)}");
+                Console.WriteLine("Magreza Grau II");
+                Console.WriteLine("Você está abaixo do peso!");
+                Console.WriteLine("Risco: ");
+            }
+            else if (IMC >= 17.0 && IMC <= 18.4)
+            {
+                Console.WriteLine($"Seu IMC é {Math.Round(IMC, 2)}");
+                Console.WriteLine("Magreza Grau I");
+                Console.WriteLine("Risco: ");
+            }
+            else if (IMC >= 18.5 && IMC <= 24.9)
+            {
+                Console.WriteLine($"Seu IMC é {Math.Round(IMC, 2)}");
+                Console.WriteLine("Eutrofia");
+                Console.WriteLine("Risco: ");
+            }
+            else if (IMC >= 25.0 && IMC <= 29.9)
+            {
+                Console.WriteLine($"Seu IMC é {Math.Round(IMC, 2)}");
+                Console.WriteLine("Sobrepeso");
+                Console.WriteLine("Risco: Aumentado");
+            }
+            else if (IMC >= 30.0 && IMC <= 34.9)
+            {
+                Console.WriteLine($"Seu IMC é {Math.Round(IMC, 2)}");
+                Console.WriteLine("Obesidade Grau I");
+                Console.WriteLine("Risco: Moderado");
+            }
+            else if (IMC >= 35.0 && IMC <= 40.0)
+            {
+                Console.WriteLine($"Seu IMC é {Math.Round(IMC, 2)}");
+                Console.WriteLine("Obesidade Grau II");
+                Console.WriteLine("Risco: Grave ");
+            }
+            else
+            {
+                Console.WriteLine($"Seu IMC é {Math.Round(IMC, 2)}");
+                Console.WriteLine("Obesidade Grau III");
+                Console.WriteLine("Você está muito acima do peso!");
+                Console.WriteLine("Risco: Muito grave");
+            }
+
+
+        }
+    }
+}
